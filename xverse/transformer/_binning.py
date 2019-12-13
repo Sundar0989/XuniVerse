c37a2c8a50fb7383ae10abe677f63b5ddf83ce64
Sundar Krishnan <sundarstyles89@gmail.com>
2019-12-13 12:27:27 -0500

    Update _binning.py

--- xverse/transformer/_binning.py
+++ xverse/transformer/_binning.py
@@ -3,14 +3,18 @@ import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 import scipy.stats.stats as stats
 import pandas.core.algorithms as algos
-from sklearn.utils.validation import check_is_fitted
+#from sklearn.utils.validation import check_is_fitted
 from sklearn.utils import check_array
 
 pd.options.mode.chained_assignment = None 
 
 class MonotonicBinning(BaseEstimator, TransformerMixin):
     
-    """Monotonically bin numeric variables based on target. The binning operation starts with the "max_bins" option. It iterates by reducing the number of bins, until it finds bins with monotonic relationship (either increasing or decreasing) between X and y. If the module is unable to find a monotonic relationship, it forcefully creates bins using the "force_bins" option.    
+    """Monotonically bin numeric variables based on target. The binning operation starts 
+    with the "max_bins" option. It iterates by reducing the number of bins, until it finds bins 
+    with monotonic relationship (either increasing or decreasing) between X and y. 
+    If the module is unable to find a monotonic relationship, it forcefully creates bins 
+    using the "force_bins" option.    
     
     Parameters
     ----------
@@ -20,13 +24,18 @@ class MonotonicBinning(BaseEstimator, TransformerMixin):
         - list of features: ['age', 'income',......]
     
     max_bins: int (default=20)
-        Maximum number of bins that can be created for any given variable. The final number of bins created will be less than or equal to this number.
+        Maximum number of bins that can be created for any given variable. The final number of bins 
+        created will be less than or equal to this number.
         
     force_bins: int (default=3)
-        It forces the module to create bins for a variable, when it cannot find monotonic relationship using "max_bins" option. The final number of bins created will be equal to the number specified.
+        It forces the module to create bins for a variable, when it cannot find monotonic relationship 
+        using "max_bins" option. The final number of bins created will be equal to the number specified.
         
     cardinality_cutoff: int (default=5)
-        Cutoff to determine if a variable is eligible for monotonic binning operation. Any variable which has unique levels less than this number will be treated as character variables. At this point no binning operation will be performed on the variable and it will return the unique levels as bins for these variable.
+        Cutoff to determine if a variable is eligible for monotonic binning operation. Any variable 
+        which has unique levels less than this number will be treated as character variables. 
+        At this point no binning operation will be performed on the variable and it will return 
+        the unique levels as bins for these variable.
     
     prefix: string (default=None)
         Variable prefix to be used for the column created by monotonic binning. 
@@ -34,7 +43,8 @@ class MonotonicBinning(BaseEstimator, TransformerMixin):
     custom_binning: dict (default=None)
         Dictionary structure - {'feature_name': float list}
         Example - {'age': [0., 1., 2., 3.]}
-        Using this parameter, the user can perform custom binning on variables. This parameter is also used to apply previously computed bins for each feature (Score new data). 
+        Using this parameter, the user can perform custom binning on variables. 
+        This parameter is also used to apply previously computed bins for each feature (Score new data). 
     """
     
     # Initialize the parameters for the function
@@ -57,7 +67,8 @@ class MonotonicBinning(BaseEstimator, TransformerMixin):
     # the fit function for monotonic binning
     def fit(self, X, y):
         
-        #if the function is used as part of pipeline, then try to unpack tuple values produced in the previous step. Added as a part of pipeline feature. 
+        #if the function is used as part of pipeline, then try to unpack tuple values 
+        #produced in the previous step. Added as a part of pipeline feature. 
         try:
             X, y = X
         except:
@@ -68,12 +79,14 @@ class MonotonicBinning(BaseEstimator, TransformerMixin):
         
         #The length of X and Y should be equal
         if X.shape[0] != y.shape[0]:
-            raise ValueError("Mismatch in input lengths. Length of X is " + str(X.shape[0]) + " but length of y is " + str(y.shape[0]) + ".")
+            raise ValueError("Mismatch in input lengths. Length of X is " + str(X.shape[0]) + " \
+                            but length of y is " + str(y.shape[0]) + ".")
         
         # The label must be binary with values {0,1}
         unique = np.unique(y)
         if len(unique) != 2:
-            raise ValueError("The target column y must be binary. But the target contains " + str(len(unique)) + " unique value(s).")
+            raise ValueError("The target column y must be binary. But the target contains " + str(len(unique)) \
+                             + " unique value(s).")
         
         #identify the variables to bin and assign the bin mapping dictionary
         self.bins = {} #bin mapping
@@ -99,15 +112,19 @@ class MonotonicBinning(BaseEstimator, TransformerMixin):
         
         #check if the dataframe is numeric. If any categorical data is present, raise errors
         try:
-            fit_X = pd.DataFrame(check_array(temp_X, accept_sparse=True, force_all_finite=False, copy=True), columns=fit_on_features)
+            fit_X = pd.DataFrame(check_array(temp_X, accept_sparse=True, force_all_finite=False, \
+                                             copy=True), columns=fit_on_features)
         except:
-            raise ValueError("The input feature(s) should be numeric type. Some of the input features has character values in it. Please use a encoder before performing monotonic operations.")
+            raise ValueError("The input feature(s) should be numeric type. Some of the input features \
+                            has character values in it. Please use a encoder before performing monotonic operations.")
         
         #apply the monotonic train function on dataset
         fit_X.apply(lambda x: self.train(x, y), axis=0)
         return self
     
-    #check the cardinality of each column present in input data. Input data X is a Pandas Series type. If the cardinality is less than user provided input, then the input data will be processed as is. No fit will be performed on the data. If the cardinality is greater, fit will be performed. 
+    #check the cardinality of each column present in input data. Input data X is a Pandas Series type. 
+    #If the cardinality is less than user provided input, then the input data will be processed as is. 
+    #No fit will be performed on the data. If the cardinality is greater, fit will be performed. 
     def check_cardinality(self, X):
         
         if len(pd.Series.unique(X)) > self.cardinality_cutoff: 
@@ -117,7 +134,8 @@ class MonotonicBinning(BaseEstimator, TransformerMixin):
 
         return fitted
     
-    #monotonic binning - The function is applied on each columns identified in the fit function. Here, the input X is a Pandas Series type.
+    #monotonic binning - The function is applied on each columns identified in the fit function. 
+    #Here, the input X is a Pandas Series type.
     def train(self, X, y):
         
         fitted = self.check_cardinality(X) #check the unique value and evaluate if fit is needed or not
@@ -129,7 +147,8 @@ class MonotonicBinning(BaseEstimator, TransformerMixin):
             max_bins = self.max_bins
             force_bins = self.force_bins
             
-            """Calculate spearman correlation for the distribution identified. If the distribution is not monotonic, reduce bins and reiterate. Proceed until either one of the following happens,
+            """Calculate spearman correlation for the distribution identified. If the distribution is not monotonic, 
+            reduce bins and reiterate. Proceed until either one of the following happens,
             a) Monotonic relationship is identified between a feature X and y is identified (np.abs(r) =1)
             b) max_bins = 0, in which case the code could not identify a monotonic relationship
             """
@@ -144,7 +163,8 @@ class MonotonicBinning(BaseEstimator, TransformerMixin):
                     max_bins = max_bins - 1
             
             """
-            Execute this block when monotonic relationship is not identified by spearman technique. We still want our code to produce bins.
+            Execute this block when monotonic relationship is not identified by spearman technique. 
+            We still want our code to produce bins.
             """
             if len(bins_X_grouped) == 1:
                 bins = algos.quantile(X, np.linspace(0, 1, force_bins)) #creates a new binnning based on forced bins
@@ -164,7 +184,8 @@ class MonotonicBinning(BaseEstimator, TransformerMixin):
     #Transform new data or existing data based on the fit identified or custom transformation provided by user
     def transform(self, X, y=None):
         
-        #if the function is used as part of pipeline, then try to unpack tuple values produced in the previous step. Added as a part of pipeline feature. 
+        #if the function is used as part of pipeline, then try to unpack tuple values produced in the 
+        #previous step. Added as a part of pipeline feature. 
         try:
             X, y = X
         except:
@@ -175,8 +196,9 @@ class MonotonicBinning(BaseEstimator, TransformerMixin):
         
         #identify the features on which the transformation should be performed
         try:
-            check_is_fitted(self, 'transform_features')
-            transform_features = self.transform_features
+            #check_is_fitted(self, 'transform_features')
+            if self.transform_features:
+                transform_features = self.transform_features
         except:
             if self.custom_binning:
                 transform_features = list(self.custom_binning.keys())
@@ -188,7 +210,8 @@ class MonotonicBinning(BaseEstimator, TransformerMixin):
         
         #raise error if the list is empty
         if not transform_features:
-            raise ValueError("Estimator has to be fitted to make monotonic transformations")
+            raise ValueError("Empty list for monotonic transformation. \
+                            Estimator has to be fitted to make monotonic transformations")
         
         #iterate through the dataframe and apply the bins
         for i in transform_features:
@@ -211,7 +234,10 @@ class MonotonicBinning(BaseEstimator, TransformerMixin):
                         self.bins = self.custom_binning
             
             #check if the bin mapping is present 
-            check_is_fitted(self, 'bins')
+            #check_is_fitted(self, 'bins')
+            if not self.bins:
+                raise ValueError("Bin variable is not present. \
+                                Estimator has to be fitted to apply monotonic transformations.")
             
             #input data cardinality check
             fitted = self.check_cardinality(tempX)
